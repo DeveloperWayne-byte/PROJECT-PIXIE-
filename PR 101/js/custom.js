00a110aae93064641e308b77ff9d3f1271e14eec
DeveloperWayne-byte <abanjhonwayne@gmail.com>
2025-03-04 09:11:31 +0800

    Add files via upload

---
+++ PR 101/js/custom.js
@@ -0,0 +1,6 @@
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+getYear();
